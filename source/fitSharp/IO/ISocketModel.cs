77ebab748232f0b3216988d92767e3af3e295cff
Igor Manushin <igor.manushin@deltacorvi.com>
2014-04-01 00:51:53 +0400

    Obsolete projects cleanup

--- source/fitSharp/IO/ISocketModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace fitSharp.IO
-{
-    public interface ISocketModel
-    {
-        int Receive(byte[] buffer, int offset, int bytesToRead);
-        void Send(byte[] buffer);
-        void Close();
-    }
-}
