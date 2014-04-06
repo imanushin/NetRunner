e38554f6173324d3175a5805db5e9a21a896fde5
Sergey Dramarecky <iron.soul.sd@gmail.com>
2014-04-06 04:36:01 +0400

    Add missing file

---
+++ source/NetRunner.Executable/Common/Utils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetRunner.Executable.Common
+{
+    internal static class Utils
+    {
+        public static string ToFinnessIntegerString(this int value)
+        {
+            return value.ToString(CultureInfo.InvariantCulture).PadLeft(FitnesseCommunicator.IntengerFitnessSize,'0');
+        }
+    }
+}
