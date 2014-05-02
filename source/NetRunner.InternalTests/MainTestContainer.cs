104425d785e79bb2be91a3dbd686789e795f11ef
Igor Manushin <igor.manushin@deltacorvi.com>
2014-05-02 11:29:05 +0400

    Internal tests execution idea

---
+++ source/NetRunner.InternalTests/MainTestContainer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using NetRunner.ExternalLibrary;
+
+namespace NetRunner.InternalTests
+{
+    internal sealed class MainTestContainer : BaseTestContainer
+    {
+        private Uri pathToFitnesseRoot;
+
+        private List<HtmlNode> tables;
+
+        public bool SetFintessePath(Uri pathToRoot)
+        {
+            try
+            {
+                var request = WebRequest.CreateHttp(pathToRoot);
+
+                using (var responce = request.GetResponse())
+                {
+                    using (var stream = responce.GetResponseStream())
+                    {
+                        using (var reader = new StreamReader(stream))
+                        {
+                            reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Trace.TraceError("Unable to access to {0}: {1}", pathToRoot, ex);
+
+                return false;
+            }
+
+            pathToFitnesseRoot = pathToRoot;
+
+            return true;
+        }
+
+        public bool ExecutePage(string pageLocalUrl)
+        {
+            /**/
+            return false;
+        }
+
+        public int ResultTableCount()
+        {
+            if (tables == null)
+            {
+                Trace.TraceError("No tests were executed");
+
+                return -1;
+            }
+
+            return tables.Count;
+        }
+    }
+}
