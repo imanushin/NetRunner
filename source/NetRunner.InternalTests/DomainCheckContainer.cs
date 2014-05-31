139109d3353cd93fd57023b7e97f35b3242ceafb
Igor Manushin <igor.manushin@deltacorvi.com>
2014-05-31 19:20:31 +0400

    +domain checking tests

---
+++ source/NetRunner.InternalTests/DomainCheckContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NetRunner.ExternalLibrary;
+
+namespace NetRunner.InternalTests
+{
+    internal sealed class DomainCheckContainer : BaseTestContainer
+    {
+        public bool CurrentDomainDoesNotContainLibrary(string assemblyName)
+        {
+            var assemblyFound = AppDomain.CurrentDomain.GetAssemblies()
+                .FirstOrDefault(a => a.FullName.IndexOf(assemblyName, StringComparison.OrdinalIgnoreCase) >= 0);
+
+            return ReferenceEquals(null, assemblyFound);
+        }
+
+        public bool CurrentDomainIdentityNotEqual(int identity)
+        {
+            return AppDomain.CurrentDomain.Id != identity;
+        }
+    }
+}
