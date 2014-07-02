a17380ba547a8b6ca60bc79c70a6dad2ddab3fe5
Igor Manushin <igor.manushin@deltacorvi.com>
2014-07-03 01:24:39 +0400

    Assembly versions refactoring

---
+++ source/CommonAssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("Any CPU")]
+[assembly: AssemblyCompany("Public")]
+[assembly: AssemblyProduct("NetRunner for FitNesse")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
