dc6541f9d8dc1ee44c82297457ba8f3ce8e77194
Igor Manushin <igor.manushin@deltacorvi.com>
2014-07-07 01:38:46 +0400

    Version fix

--- source/CommonAssemblyInfo.cs
+++ source/CommonAssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.2")]
 [assembly: AssemblyFileVersion("1.0.2")]
