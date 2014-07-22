992c28277a3996e8a454787e9f397cbf8f6bc7e4
Igor Manushin <igor.manushin@deltacorvi.com>
2014-07-23 00:51:39 +0400

    +EmbeddedTestContainer help

--- source/NetRunner.ExternalLibrary/EmbeddedTestContainer.cs
+++ source/NetRunner.ExternalLibrary/EmbeddedTestContainer.cs
@@ -6,8 +6,16 @@ using System.Text;
 
 namespace NetRunner.ExternalLibrary
 {
+    /// <summary>
+    /// Embedded tests. They are fully supported on the all platforms where NetRunner can work.
+    /// </summary>
     public sealed class EmbeddedTestContainer : BaseTestContainer
     {
+        /// <summary>
+        /// Invokes the Debugger.Launch() method.<br/>
+        /// Usage:<br/>
+        /// | ''' NetRunner:Launch Debugger ''' |
+        /// </summary>
         [ExcludeDefaultFunctionName]
         [AdditionalFunctionName("NetRunner:Launch Debugger")]
         public void DebbugerBreak()
