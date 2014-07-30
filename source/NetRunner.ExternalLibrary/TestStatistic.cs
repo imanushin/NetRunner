dc5d00bcabc99c2cd78d25bc2584681101380330
Igor Manushin <igor.manushin@deltacorvi.com>
2014-07-31 00:59:08 +0400

    +test statistic help

--- source/NetRunner.ExternalLibrary/TestStatistic.cs
+++ source/NetRunner.ExternalLibrary/TestStatistic.cs
@@ -6,12 +6,16 @@ using NetRunner.ExternalLibrary.Properties;
 
 namespace NetRunner.ExternalLibrary
 {
+    /// <summary>
+    /// Test statistic of the functions executed.
+    /// This is readonly class, to use property <see cref="GlobalStatistic"/> to get the latest execution status
+    /// </summary>
     public sealed class TestStatistic
     {
         [UsedImplicitly]
         internal static TestStatistic GlobalStatisticInternal = new TestStatistic(0, 0, 0, 0);
 
-        public TestStatistic(int right, int wrong, int skipped, int errors)
+        internal TestStatistic(int right, int wrong, int skipped, int errors)
         {
             Right = right;
             Wrong = wrong;
@@ -19,30 +23,45 @@ namespace NetRunner.ExternalLibrary
             Errors = errors;
         }
 
+        /// <summary>
+        /// Count of right tests
+        /// </summary>
         public int Right
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// Count of wrong tests (e.g. tests which executed without exception and returned incorrect result)
+        /// </summary>
         public int Wrong
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// Skipped tests count
+        /// </summary>
         public int Skipped
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// Count of tests executed with exception
+        /// </summary>
         public int Errors
         {
             get;
             private set;
         }
 
+        /// <summary>
+        /// Current statistic. After each test invokation it is changed to the other, so read this property to get the latest information
+        /// </summary>
         public static TestStatistic GlobalStatistic
         {
             get
